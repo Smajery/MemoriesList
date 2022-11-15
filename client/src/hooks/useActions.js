2c38d990cae6b9136fd02e68b0050d619bd6960f
Smajery <vasiliew.sasha@gmail.com>
2022-11-15 16:11:18 +0100

    Переместил файлы в папку

--- client/src/hooks/useActions.js
+++
@@ -1,8 +0,0 @@
-import {useDispatch} from "react-redux";
-import {bindActionCreators} from "redux";
-import {AllActionCreators} from "../store/reducers/action-creators";
-
-export const useActions = () => {
-    const dispatch = useDispatch()
-    return bindActionCreators(AllActionCreators, dispatch)
-}
--- client/src/store/reducers/action-creators.js
+++
@@ -1,7 +0,0 @@
-import {AuthActionCreator} from "./auth/action-creator";
-import {MemoryActionCreator} from "./memory/action-creator";
-
-export const AllActionCreators  = {
-    ...AuthActionCreator,
-    ...MemoryActionCreator,
-}
